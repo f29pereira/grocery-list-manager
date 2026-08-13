fd67fe4a2121f1e2410eb6a93ff4616188b302f3
Filipe Pereira <f29.pereira@gmail.com>
2026-08-13 10:14:42 +0100

    chore: rename index.css file to globals
    - Update main.tsx import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/assets/styles/index.css";
+import "@/assets/styles/globals.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 

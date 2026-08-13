f65f2eac7595b010d0edc2de35d9ab6e52a2cada
Filipe Pereira <f29.pereira@gmail.com>
2026-08-13 17:03:14 +0100

    feat: update icons file

--- src/assets/icons/icon.ts
+++ src/assets/icons/icon.ts
@@ -1,4 +1,14 @@
-import { IoMdMoon, IoMdSunny } from "react-icons/io";
+import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
+import { IoIosClose, IoMdMoon, IoMdSunny } from "react-icons/io";
+import { RxHamburgerMenu } from "react-icons/rx";
 import { TbPaperBag } from "react-icons/tb";
 
-export { TbPaperBag, IoMdMoon, IoMdSunny };
+export {
+  FaRegUserCircle,
+  FaUserCircle,
+  IoIosClose,
+  IoMdMoon,
+  IoMdSunny,
+  RxHamburgerMenu,
+  TbPaperBag,
+};

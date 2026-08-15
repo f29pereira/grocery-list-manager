b7bf705fe3f8f5f57a8bb7fcc2d48ba66d4ca23b
Filipe Pereira <f29.pereira@gmail.com>
2026-08-15 16:15:05 +0100

    feat: update MobileNavToggle component styles
    Hamburger button:
    - Remove the usage of clsx (not needed for this element)
    - Visible focus: Add blue-500 outline and blue-50 background color
    - Increase the icon size to 30px for viewports >= 768px

--- src/components/shared/Nav/MobileNav/MobileNavToggle/MobileNavToggle.tsx
+++ src/components/shared/Nav/MobileNav/MobileNavToggle/MobileNavToggle.tsx
@@ -1,7 +1,6 @@
 import useToggle from "@/hooks/useToggle";
 import { RxHamburgerMenu } from "@/assets/icons/icon";
 import MobileNav from "../MobileNav";
-import clsx from "clsx";
 
 /**
  * Renders a hamburger button or dialog (MobileNav component)
@@ -12,17 +11,18 @@ export default function MobileNavToggle() {
   return (
     <>
       <button
-        className={clsx(
-          "flex items-center",
-          "cursor-pointer text-green-600",
-          "focus-visible:outline-solid focus-visible:outline-[3.2px] focus-visible:outline-offset-4 focus-visible:outline-green-900",
-          "transition-[color] delay-150 duration-300 ease-in-out motion-reduce:transition-none md:hover:text-green-500",
-        )}
+        className="flex items-center
+          cursor-pointer rounded-sm text-green-600
+          focus-visible:outline-solid focus-visible:outline-[3.2px] focus-visible:outline-offset-2
+        focus-visible:outline-blue-500 focus-visible:bg-blue-50
+          transition-[color] delay-150 duration-300 ease-in-out 
+          motion-reduce:transition-none 
+        hover:text-green-500"
         onClick={toggle}
         aria-label="Mobile navigation"
         aria-expanded={isToggled}
       >
-        <RxHamburgerMenu className="text-2xl " aria-hidden="true" />
+        <RxHamburgerMenu className="text-2xl md:text-3xl" aria-hidden="true" />
       </button>
 
       <MobileNav isToggled={isToggled} setToggle={setIsToggled} />

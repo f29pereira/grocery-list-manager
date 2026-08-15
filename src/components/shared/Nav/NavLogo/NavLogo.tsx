ab4b0609809066ce8c5b40a6df3943c17e6d649e
Filipe Pereira <f29.pereira@gmail.com>
2026-08-15 16:31:47 +0100

    feat: add NavLogo component
    - Add component NavLogo to render the app logo used by the Nav component

---
+++ src/components/shared/Nav/NavLogo/NavLogo.tsx
@@ -0,0 +1,48 @@
+import { TbPaperBag } from "@/assets/icons/icon";
+import NavigationLink from "@/components/ui/NavigationLink/NavigationLink";
+
+/**
+ * Renders the app logo, used by the Nav component, as a navigation link
+ *
+ * Displays:
+ * - the logo icon and "GM" on viewports < 640px
+ * - the logo icon and "Grocery List" viewports >= 640px
+ */
+export default function NavLogo() {
+  return (
+    <>
+      {/*Viewports < 640px*/}
+      <NavigationLink
+        styles="text-green-600
+                font-space-grotesk font-black
+                transition-[color] delay-150 duration-300 ease-in-out 
+                motion-reduce:transition-none 
+                hover:text-green-500
+                sm:hidden"
+        to="/"
+      >
+        <div className="flex justify-center items-center gap-1">
+          <TbPaperBag className="text-2xl sm:text-3xl" />
+          <span className="text-2xl tracking-widest">GM</span>
+        </div>
+      </NavigationLink>
+
+      {/*Viewports >= 640px*/}
+      <NavigationLink
+        styles="hidden
+                text-green-600
+                font-space-grotesk font-black
+                transition-[color] delay-150 duration-300 ease-in-out 
+                motion-reduce:transition-none 
+                hover:text-green-500
+                sm:inline-block"
+        to="/"
+      >
+        <div className="flex justify-center items-center gap-2">
+          <TbPaperBag className="text-2xl md:text-3xl" />
+          <span className="text-2xl md:text-3xl">Grocery List</span>
+        </div>
+      </NavigationLink>
+    </>
+  );
+}

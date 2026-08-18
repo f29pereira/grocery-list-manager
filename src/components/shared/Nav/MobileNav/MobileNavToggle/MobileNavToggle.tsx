caa81f34b1ded84021ec21a0b7c7d0cd30373a3b
Filipe Pereira <f29.pereira@gmail.com>
2026-08-18 16:32:17 +0100

    feat: update MobileNavToggle component
    - Update component docs
    - Replace React fragment with div that is hidden for viewports < 1024px

--- src/components/shared/Nav/MobileNav/MobileNavToggle/MobileNavToggle.tsx
+++ src/components/shared/Nav/MobileNav/MobileNavToggle/MobileNavToggle.tsx
@@ -3,21 +3,21 @@ import { RxHamburgerMenu } from "@/assets/icons/icon";
 import MobileNav from "../MobileNav";
 
 /**
- * Renders a hamburger button or dialog (MobileNav component)
+ * Renders a hamburger button or dialog (MobileNav component) on viewports < 1024px
  */
 export default function MobileNavToggle() {
   const { isToggled, toggle, setIsToggled } = useToggle(false);
 
   return (
-    <>
+    <div className="lg:hidden">
       <button
         className="flex items-center
-          cursor-pointer rounded-sm text-green-600
-          focus-visible:outline-solid focus-visible:outline-[3.2px] focus-visible:outline-offset-2
-        focus-visible:outline-blue-500 focus-visible:bg-blue-50
-          transition-[color] delay-150 duration-300 ease-in-out 
-          motion-reduce:transition-none 
-        hover:text-green-500"
+                  cursor-pointer rounded-sm text-green-600
+                  focus-visible:outline-solid focus-visible:outline-[3.2px] focus-visible:outline-offset-2
+                focus-visible:outline-blue-500 focus-visible:bg-blue-50
+                  transition-[color] delay-150 duration-300 ease-in-out 
+                  motion-reduce:transition-none 
+                hover:text-green-500"
         onClick={toggle}
         aria-label="Mobile navigation"
         aria-expanded={isToggled}
@@ -26,6 +26,6 @@ export default function MobileNavToggle() {
       </button>
 
       <MobileNav isToggled={isToggled} setToggle={setIsToggled} />
-    </>
+    </div>
   );
 }

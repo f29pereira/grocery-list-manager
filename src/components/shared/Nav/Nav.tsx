a7c4c70774bfb25edc06af0b6704f79c68f9aec1
Filipe Pereira <f29.pereira@gmail.com>
2026-08-18 16:38:03 +0100

    feat: update Nav component
    - Integrate the components: MobileNavToggle, NavLogo, DesktopLinksList
    - Add placeholder button for the user profile

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -1,6 +1,34 @@
+import { FaRegUserCircle } from "@/assets/icons/icon";
+import MobileNavToggle from "./MobileNav/MobileNavToggle/MobileNavToggle";
+import NavLogo from "./NavLogo/NavLogo";
+import DesktopLinksList from "./DesktopLinksList/DesktopLinksList";
+
 /**
- * Renders the app navigation
+ * Renders the app navigation with:
+ * - Mobile navigation toggle on viewports < 1024px
+ * - Logo
+ * - Desktop navigation links on viewports >= 1024px
+ * - User profile button
  */
 export default function Nav() {
-  return <div>Nav component</div>;
+  return (
+    <nav
+      className="relative px-6 py-5 flex justify-between items-center 
+      bg-white
+        md:px-8 md:py-7"
+      aria-label="Main"
+    >
+      <MobileNavToggle />
+
+      <div className="flex items-center gap-20">
+        <NavLogo />
+
+        <DesktopLinksList />
+      </div>
+
+      <button>
+        <FaRegUserCircle className="cursor-pointer text-2xl text-green-600 md:hover:text-green-500 md:text-3xl" />
+      </button>
+    </nav>
+  );
 }

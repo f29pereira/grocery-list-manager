0ffd10fc05aea88d3e5e50af2a291ff0d1245d69
Filipe Pereira <f29.pereira@gmail.com>
2026-08-20 15:40:45 +0100

    feat: integrate the components FooterLogo, FooterNav and FooterCredit on Footer

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -1,6 +1,27 @@
+import FooterLogo from "./FooterLogo/FooterLogo";
+import FooterNav from "./FooterNav/FooterNav";
+import FooterCredit from "./FooterCredit/FooterCredit";
+
 /**
- * Renders the app footer
+ * Renders the app footer with:
+ * - Logo
+ * - Links: Company, Legal, Support and Social
+ * - Footer credit
  */
 export default function Footer() {
-  return <div>Footer component</div>;
+  return (
+    <footer
+      className="bg-white py-12
+                lg:px-10 lg:py-20
+                xl:px-32"
+    >
+      <div className="lg:flex lg:justify-between">
+        <FooterLogo />
+
+        <FooterNav />
+      </div>
+
+      <FooterCredit />
+    </footer>
+  );
 }

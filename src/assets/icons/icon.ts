7e037daddd5c826eb4c3cd70e9d6e0b9f755ad32
Filipe Pereira <f29.pereira@gmail.com>
2026-08-20 15:32:55 +0100

    feat: update icons.ts file with new icons
    - Add icons for: socials X, Facebook and Instragram
    - Add copyright icon

--- src/assets/icons/icon.ts
+++ src/assets/icons/icon.ts
@@ -1,4 +1,5 @@
-import { FaRegUserCircle, FaUserCircle } from "react-icons/fa";
+import { FaRegUserCircle, FaUserCircle, FaRegCopyright } from "react-icons/fa";
+import { FaXTwitter, FaFacebook, FaSquareInstagram } from "react-icons/fa6";
 import { IoIosClose, IoMdMoon, IoMdSunny } from "react-icons/io";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { TbPaperBag } from "react-icons/tb";
@@ -6,6 +7,10 @@ import { TbPaperBag } from "react-icons/tb";
 export {
   FaRegUserCircle,
   FaUserCircle,
+  FaXTwitter,
+  FaFacebook,
+  FaSquareInstagram,
+  FaRegCopyright,
   IoIosClose,
   IoMdMoon,
   IoMdSunny,

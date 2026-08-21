db5334e4dba5daf2ee0cef4c4534225fd6038353
Filipe Pereira <f29.pereira@gmail.com>
2026-08-21 21:14:34 +0100

    chore: add common.types.ts file
    - Add common.types.ts for the TypeScript types used throughout the app

---
+++ src/types/common.types.ts
@@ -0,0 +1,9 @@
+import type { ReactNode } from "react";
+
+/**
+ * Type for React children
+ * @property children  - single or list of React children
+ */
+export type ReactChildrenType = {
+  children: ReactNode;
+};

8105ba65a701c1d5f1df1cbb4e8d8f08e654b9e6
Filipe Pereira <f29.pereira@gmail.com>
2026-08-22 17:37:20 +0100

    feat: integrate ThemeSwitcher into Nav and update Nav styles
    - Integrate ThemeSwitcher into Nav to be displayed for viewports >= 1024px
    - Add light and dark theme variants

--- src/components/shared/Nav/Nav.tsx
+++ src/components/shared/Nav/Nav.tsx
@@ -2,20 +2,22 @@ import { FaRegUserCircle } from "@/assets/icons/icon";
 import MobileNavToggle from "./MobileNav/MobileNavToggle/MobileNavToggle";
 import NavLogo from "./NavLogo/NavLogo";
 import DesktopLinksList from "./DesktopLinksList/DesktopLinksList";
+import ThemeSwitcher from "@/components/ui/ThemeSwitcher/ThemeSwitcher";
 
 /**
  * Renders the app navigation with:
  * - Mobile navigation toggle on viewports < 1024px
  * - Logo
  * - Desktop navigation links on viewports >= 1024px
+ * - Theme switch on on viewports >= 1024px
  * - User profile button
  */
 export default function Nav() {
   return (
     <nav
-      className="relative px-6 py-5 flex justify-between items-center 
-      bg-white
-        md:px-8 md:py-7"
+      className="relative flex justify-between items-center px-6 py-5
+              bg-nav-footer-bg dark:bg-dark-nav-footer-bg theme-transition
+                md:px-8 md:py-7"
       aria-label="Main"
     >
       <MobileNavToggle />
@@ -26,9 +28,20 @@ export default function Nav() {
         <DesktopLinksList />
       </div>
 
-      <button>
-        <FaRegUserCircle className="cursor-pointer text-2xl text-green-600 md:hover:text-green-500 md:text-3xl" />
-      </button>
+      <div className="flex items-center gap-8">
+        <div className="hidden lg:block">
+          <ThemeSwitcher />
+        </div>
+
+        <button>
+          <FaRegUserCircle
+            className="cursor-pointer text-2xl 
+                    text-brand dark:text-brand
+                    hover:text-brand-hover hover:dark:text-brand-hover
+                      md:text-3xl"
+          />
+        </button>
+      </div>
     </nav>
   );
 }

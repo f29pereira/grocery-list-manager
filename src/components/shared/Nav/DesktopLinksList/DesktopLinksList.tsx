a6262868bcc8fc23ea0f3edc7e466f804e7cba2d
Filipe Pereira <f29.pereira@gmail.com>
2026-08-22 18:51:28 +0100

    refactor: DesktopLinksList component
    - Replace `text-base` with `text-link` and remove `text-slate-800`
    - For the `after` property: replace `h-[3px]` with `h-0.75` replace `bg-green-600` with `bg-brand`
    - For the `hover` property replace `text-slate-700` with `text-link-hover`

--- src/components/shared/Nav/DesktopLinksList/DesktopLinksList.tsx
+++ src/components/shared/Nav/DesktopLinksList/DesktopLinksList.tsx
@@ -8,16 +8,16 @@ import NavigationLink from "@/components/ui/NavigationLink/NavigationLink";
  */
 export default function DesktopLinksList() {
   const navItemStyles = `relative inline-block
-                        text-base text-slate-800
+                        text-link
                         after:absolute after:-bottom-[9px] after:left-0
-                        after:content-[''] after:w-full after:h-[3px]
-                        after:bg-green-600 
+                        after:content-[''] after:w-full after:h-0.75
+                        after:bg-brand
                         after:[clip-path:inset(0_100%_0_0)]
                         after:transition-[clip-path]
                         after:duration-[350ms]
                         after:ease-[cubic-bezier(0.65,0,0.35,1)]
                         hover:after:[clip-path:inset(0_0_0_0)]
-                        hover:text-slate-700
+                        hover:text-link-hover
                         aria-[current=page]:after:[clip-path:inset(0_0_0_0)]
                         motion-reduce:transition-none
                         lg:mr-10`;

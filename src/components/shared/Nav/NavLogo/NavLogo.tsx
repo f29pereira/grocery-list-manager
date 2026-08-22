db4745bea640e4b1572c440a83cdb27e66166810
Filipe Pereira <f29.pereira@gmail.com>
2026-08-22 18:39:17 +0100

    refactor: NavLogo styles
    - Replace `text-green-600` with `text-brand`
    - Replace color transition properties with `theme-transition`
    - Replace `text-green-500` with `text-brand-hover`

--- src/components/shared/Nav/NavLogo/NavLogo.tsx
+++ src/components/shared/Nav/NavLogo/NavLogo.tsx
@@ -14,11 +14,10 @@ export default function NavLogo() {
       {/*Viewports < 640px*/}
       <NavigationLink
         styles="inline-block
-                text-green-600
+                text-brand
                 font-space-grotesk font-black
-                transition-[color] delay-150 duration-300 ease-in-out 
-                motion-reduce:transition-none 
-                hover:text-green-500
+                theme-transition
+                hover:text-brand-hover
                 sm:hidden"
         to="/"
       >
@@ -34,11 +33,10 @@ export default function NavLogo() {
       {/*Viewports >= 640px*/}
       <NavigationLink
         styles="hidden
-                text-green-600
+                text-brand
                 font-space-grotesk font-black
-                transition-[color] delay-150 duration-300 ease-in-out 
-                motion-reduce:transition-none 
-                hover:text-green-500
+                theme-transition 
+                hover:text-brand-hover
                 sm:inline-block"
         to="/"
       >

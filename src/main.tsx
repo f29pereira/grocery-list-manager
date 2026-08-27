20e3e1761a23bc74510eb2d20d834fdd6dfbba17
Filipe Pereira <f29.pereira@gmail.com>
2026-08-27 18:36:45 +0100

    feat: Integrate LanguageProvider and i18next import into main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,16 @@ import "@/assets/styles/globals.css";
 import { RouterProvider } from "react-router";
 import { router } from "./router";
 import ThemeProvider from "./contexts/ThemeContext/ThemeProvider";
+import LanguageProvider from "./contexts/LanguageContext/LanguageProvider";
+import "./lib/i18n/i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {
       <ThemeProvider>
-        <RouterProvider router={router} />
+        <LanguageProvider>
+          <RouterProvider router={router} />
+        </LanguageProvider>
       </ThemeProvider>
     }
   </StrictMode>,

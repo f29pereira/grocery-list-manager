60ec7f2dc567f9641a89f83058850f6d31c0fc69
Filipe Pereira <f29.pereira@gmail.com>
2026-08-31 15:41:32 +0100

    feat: Update FooterCredit styles
    - Increase top margin for the main container
    - For viewports < 1024px display the text "Developed by f29.pereira" on a new centered line
    - Make font for the text "f29.pereira" always bold

--- src/components/shared/Footer/FooterCredit/FooterCredit.tsx
+++ src/components/shared/Footer/FooterCredit/FooterCredit.tsx
@@ -13,18 +13,25 @@ export default function FooterCredit() {
 
   return (
     <div
-      className="flex justify-center items-center mt-10 
-                font-bold text-slate-700 dark:text-slate-300 text-sm 
+      className="mt-20
+                font-bold 
+              text-slate-700 dark:text-slate-300 text-sm 
                 lg:mt-44 lg:font-normal lg:text-base
                 xl:mt-50"
     >
-      <span className="sr-only">{t("footer.credits.copyright")}</span>
-      <FaRegCopyright className="text-lg mr-2" aria-hidden="true" />
-      <span className="mr-2">2026</span>
-      <span className="mr-2">Grocery List</span>
-      <span className="mr-1">{t("footer.credits.made-by")}</span>
-      <a
-        className="rounded-sm
+      <div className="flex flex-col lg:flex-row lg:justify-center">
+        <div className="flex justify-center items-center mb-4 lg:mb-0">
+          <span className="sr-only">{t("footer.credits.copyright")}</span>
+          <FaRegCopyright className="text-lg mr-2" aria-hidden="true" />
+          <span className="mr-2">2026</span>
+          <span className="mr-2">Grocery List</span>
+        </div>
+
+        <div className="flex justify-center items-center">
+          <span className="mr-1">{t("footer.credits.made-by")}</span>
+          <a
+            className="rounded-sm
+                  font-bold
                   text-base text-link
                   focus-visible:focus-ring focus-visible:outline-offset-2
                   focus-visible:dark:bg-transparent
@@ -32,13 +39,15 @@ export default function FooterCredit() {
                 hover:text-link-hover 
                   hover:underline hover:underline-offset-8 
                   hover:decoration-text-link"
-        href="https://github.com/f29pereira"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        f29.pereira
-        <span className="sr-only">{t("footer.credits.new-tab")}</span>
-      </a>
+            href="https://github.com/f29pereira"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            f29.pereira
+            <span className="sr-only">{t("footer.credits.new-tab")}</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }

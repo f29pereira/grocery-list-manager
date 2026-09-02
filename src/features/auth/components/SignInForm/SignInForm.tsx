347949b11fbf9b7d1acba329d9480530249db848
Filipe Pereira <f29.pereira@gmail.com>
2026-09-02 18:27:10 +0100

    feat: Add components SignInForm and SignIn
    - Add component SignInForm that will render, in a later commit, the form for creating an user account
    - Add route component SignIn to render SignInForm

---
+++ src/features/auth/components/SignInForm/SignInForm.tsx
@@ -0,0 +1,8 @@
+/**
+ * Renders the user sign in form with:
+ * TO DO
+ */
+export default function SignInForm() {
+  // TO DO: Add form
+  return <p>Sign In Form</p>;
+}
---
+++ src/routes/signIn.tsx
@@ -0,0 +1,8 @@
+import SignInForm from "@/features/auth/components/SignInForm/SignInForm";
+
+/**
+ * Renders the SignInForm component
+ */
+export default function SignIn() {
+  return <SignInForm />;
+}

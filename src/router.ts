d6c14ce198a4acffeea3096bb0fa7a39f51bb880
Filipe Pereira <f29.pereira@gmail.com>
2026-09-02 18:28:48 +0100

    feat: Update router.ts
    - Add authentication route "signIn"
    - Add generic page not found route

--- src/router.ts
+++ src/router.ts
@@ -1,19 +1,32 @@
 import { createBrowserRouter } from "react-router";
 import Root from "./routes/root";
 import RootErrorBoundary from "./routes/rootErrorBoundary";
+import SignIn from "./routes/signIn";
+import AuthLayout from "./routes/authLayout";
 
 /**
  * Routes configuration
  */
 export const router = createBrowserRouter([
+  // Authentication routes
+  {
+    Component: AuthLayout,
+    children: [
+      { path: "signIn", Component: SignIn } /*TO DO: Add "signup" route*/,
+    ],
+  },
+
+  // Root
   {
     path: "/",
     Component: Root,
-    ErrorBoundary: RootErrorBoundary,
     children: [
-      // TO DO: Add Home component
-      //{ index: true, Component: Home }, // renders at "/"
-      //{ path: "toDo", Component: ToDo }, // renders at "/toDo"
+      // TO DO: Add "home" route
+      // Protected routes
+      // TO DO: Add protected routes
     ],
   },
+
+  // Page Not Found
+  { path: "*", Component: RootErrorBoundary },
 ]);

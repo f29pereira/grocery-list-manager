85f61ad66278446d5c55e2a71732719528b85fba
Filipe Pereira <f29.pereira@gmail.com>
2026-09-13 11:45:35 +0100

    feat: Add NextStepButton component
    - Add component NextStepButton to render a pill button to go to the next step within a MultiStepProvider

---
+++ src/components/ui/StepButtons/NextStepButton/NextStepButton.tsx
@@ -0,0 +1,39 @@
+import { useMultiStep } from "@/contexts/MultiStepContext/useMultiStep";
+import { useTranslation } from "react-i18next";
+import PillButton from "../../PillButton/PillButton";
+import { IoIosArrowForward } from "@/assets/icons/icon";
+
+/**
+ * Renders a next step button
+ *
+ * Can be used within a MultiStepProvider
+ */
+export default function NextStepButton() {
+  // Translation
+  const { t } = useTranslation();
+
+  // Context
+  const { nextStep } = useMultiStep();
+
+  return (
+    <PillButton
+      styles="bg-button group
+              shadow-lg shadow-green-700/50
+              hover:bg-button-hover"
+      handleClick={nextStep}
+    >
+      <div className="flex justify-center items-center gap-2">
+        <span className="font-black text-lg text-button-label">
+          {t("multi-step.next-step")}
+        </span>
+        <IoIosArrowForward
+          className="text-3xl text-button-label
+                    transition-transform duration-300
+                    group-hover:scale-125
+                    motion-reduce:transition-none"
+          aria-hidden="true"
+        />
+      </div>
+    </PillButton>
+  );
+}

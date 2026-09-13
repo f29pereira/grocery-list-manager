f9af5ae818df8d4c63dd342297a642585e2abf2e
Filipe Pereira <f29.pereira@gmail.com>
2026-09-13 16:50:42 +0100

    feat: Add SignUpForm component
    - Add component SignUpForm to render the multi-step sign up form
    - Add SignUpForm.utils with function getStepsDescription that returns the steps descriptions

---
+++ src/features/auth/components/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,56 @@
+import Logo from "@/components/shared/Logo/Logo";
+import StepsList from "@/components/shared/StepsList/StepsList";
+import AuthStep from "./Steps/AuthStep/AuthStep";
+import EmailVerificationStep from "./Steps/EmailVerificationStep/EmailVerificationStep";
+import { useTranslation } from "react-i18next";
+import { useMultiStep } from "@/contexts/MultiStepContext/useMultiStep";
+import { getStepsDescription } from "./SignUpForm.utils";
+
+/**
+ * Renders the multi-step sign up form with:
+ * - List of steps
+ * - Authentication step
+ * - Email Verification step
+ */
+export default function SignUpForm() {
+  // Translation
+  const { t } = useTranslation();
+
+  // Context
+  const { currentStep } = useMultiStep();
+
+  // Data
+  const stepsList = getStepsDescription(t);
+
+  return (
+    <div
+      className="min-h-screen px-6 py-10
+                md:px-10
+                lg:flex lg:justify-center lg:items-center lg:p-0"
+    >
+      <div
+        className="px-6 py-10 overflow-hidden
+                bg-card 
+                  rounded-3xl 
+                  shadow-xl dark:shadow-none
+                  sm:px-10 md:px-20
+                  lg:w-200 lg:h-187.5 lg:px-10"
+      >
+        <div className="lg:flex lg:gap-40">
+          <div className="lg:flex lg:flex-col lg:gap-4">
+            <div className="flex justify-center lg:flex-none">
+              <Logo styles="text-brand hover:text-brand-hover" />
+            </div>
+            <StepsList stepsList={stepsList} currentStep={currentStep} />
+          </div>
+
+          <div className="lg:flex-1">
+            {currentStep === 0 && <AuthStep />}
+            {currentStep === 1 && <EmailVerificationStep />}
+            {/*TO DO: Add User Details step*/}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/features/auth/components/SignUpForm/SignUpForm.utils.tsx
@@ -0,0 +1,12 @@
+import type { TFunction } from "i18next";
+
+/**
+ * Returns the sign up forms descriptions
+ * @param t steps translation
+ */
+export const getStepsDescription = (
+  t: TFunction<"translation", undefined>,
+): string[] => {
+  // TO DO: Add next steps descriptions
+  return [t("forms.signUp.auth-step.step"), t("forms.signUp.email-step.step")];
+};

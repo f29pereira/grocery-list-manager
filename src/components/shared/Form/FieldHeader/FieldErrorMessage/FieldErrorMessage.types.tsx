db77586b9679941dad75bc1a84265324780f2074
Filipe Pereira <f29.pereira@gmail.com>
2026-09-15 14:30:29 +0100

    chore: move the FieldErrorMessage component to the FieldHeader folder

--- src/components/shared/Form/FieldHeader/FieldErrorMessage/FieldErrorMessage.types.tsx
+++ src/components/shared/Form/FieldHeader/FieldErrorMessage/FieldErrorMessage.types.tsx
@@ -1,6 +1,6 @@
 /**
  * Props for the FieldErrorMessage component
- * @propery  id      - id to match the field aria-describedby property
+ * @propery  id      - id to match the input field aria-describedby property
  * @property message - error message
  */
 export type FieldErrorMessageProps = {

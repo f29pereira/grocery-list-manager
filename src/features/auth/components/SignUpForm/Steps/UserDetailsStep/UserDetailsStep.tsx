c70d7e7915e13658914190c0b035153a4b9d5688
Filipe Pereira <f29.pereira@gmail.com>
2026-09-15 20:41:59 +0100

    feat: Add UserDetailStep component
    - Add component UserDetailStep to render the user details step used by the SignUpForm component

---
+++ src/features/auth/components/SignUpForm/Steps/UserDetailsStep/UserDetailsStep.tsx
@@ -0,0 +1,34 @@
+import useFocus from "@/hooks/useFocus";
+import { useTranslation } from "react-i18next";
+import AddUserDetailsForm from "./AddUserDetailsForm/AddUserDetailsForm";
+
+/**
+ * Renders the user details step used by the SignUpForm component
+ */
+export default function UserDetailStep() {
+  // Translation
+  const { t } = useTranslation();
+
+  // Custom Hook
+  const { elementRef } = useFocus<HTMLHeadingElement>();
+
+  return (
+    <div>
+      {/*Main title*/}
+      <h1
+        className="mb-8
+                  font-black 
+                  text-center text-xl text-title
+                  focus-visible:outline-none
+                  lg:text-left lg:text-2xl"
+        ref={elementRef}
+        tabIndex={-1}
+        aria-label={t("forms.signUp.user-details-step.title-label")}
+      >
+        {t("forms.signUp.user-details-step.title")}
+      </h1>
+
+      <AddUserDetailsForm />
+    </div>
+  );
+}

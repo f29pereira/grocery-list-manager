2fbad9b5e2a64607b2c0bd6b66049dd3a350a1b8
Filipe Pereira <f29.pereira@gmail.com>
2026-09-15 16:16:35 +0100

    feat: Update SubmitButton props and add a loading icon
    - Add the props: styles, isSubmitting and buttonIcon
    - Add loading icon with spin animation if the associated form is submitting

--- src/components/shared/Form/SubmitButton/SubmitButton.tsx
+++ src/components/shared/Form/SubmitButton/SubmitButton.tsx
@@ -1,17 +1,43 @@
+import clsx from "clsx";
 import type { SubmitButtonProps } from "./SubmitButton.types";
 import PillButton from "@/components/ui/PillButton/PillButton";
+import { FaCircleNotch } from "@/assets/icons/icon";
 
 /**
  * Renders a form submit pill button
+ *
+ * If the associated form is submitting, displays a loading icon with spin animation instead of the buttonIcon prop
+ *
+ * Props are defined in {@link SubmitButtonProps}.
  */
-export default function SubmitButton({ children }: SubmitButtonProps) {
+export default function SubmitButton({
+  styles,
+  isSubmitting,
+  buttonIcon,
+  children,
+}: SubmitButtonProps) {
   return (
     <PillButton
-      styles="bg-button
-            shadow-lg shadow-green-600/50 
-            hover:bg-button-hover"
+      styles={clsx(
+        "bg-button",
+        "shadow-lg shadow-green-600/50",
+        "hover:bg-button-hover",
+        styles,
+      )}
     >
-      {children}
+      <div className="flex justify-center items-center gap-4">
+        {isSubmitting ? (
+          <FaCircleNotch
+            className="text-2xl text-button-label 
+                        motion-safe:animate-spin"
+            aria-hidden="true"
+          />
+        ) : (
+          buttonIcon
+        )}
+
+        {children}
+      </div>
     </PillButton>
   );
 }
--- src/components/shared/Form/SubmitButton/SubmitButton.types.ts
+++ src/components/shared/Form/SubmitButton/SubmitButton.types.ts
@@ -2,8 +2,14 @@ import type { ReactNode } from "react";
 
 /**
  * Props for the SubmitButton component
- * @property children - button content
+ * @property styles       - (optional) Tailwind CSS classes
+ * @property isSubmitting - is the form submitting
+ * @property buttonIcon   - (optional) button icon to be displayed when the form is not submitting
+ * @property children     - button content
  */
 export type SubmitButtonProps = {
+  styles?: string;
+  isSubmitting: boolean;
+  buttonIcon?: ReactNode;
   children: ReactNode;
 };

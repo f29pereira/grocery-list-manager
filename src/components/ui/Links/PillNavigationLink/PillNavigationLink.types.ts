63c696c765be5637f6204cb03d39c4a74f043660
Filipe Pereira <f29.pereira@gmail.com>
2026-09-22 16:44:51 +0100

    feat: Add PillNavigationLink component
    - Add component PillNavigationLink to render a pill navigation link

---
+++ src/components/ui/Links/PillNavigationLink/PillNavigationLink.types.ts
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+
+/**
+ * Props for the PillNavigationLink component
+ * @property styles    - (optional) Tailwind CSS classes
+ * @property to        - route to navigate to
+ * @property ariaLabel - aria-label text description
+ * @property children  - link content
+ */
+export type PillNavigationLinkProps = {
+  styles?: string;
+  to: string;
+  ariaLabel?: string;
+  children: ReactNode;
+};

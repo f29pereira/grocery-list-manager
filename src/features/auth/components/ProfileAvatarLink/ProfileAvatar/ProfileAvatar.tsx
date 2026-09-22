43d31493f0ab6c9e8bc70d06039714b4b747785e
Filipe Pereira <f29.pereira@gmail.com>
2026-09-22 10:48:09 +0100

    feat: Update ProfileAvatar to display a different user icon if the user is authenticated but the user profile is not complete

--- src/features/auth/components/ProfileAvatarLink/ProfileAvatar/ProfileAvatar.tsx
+++ src/features/auth/components/ProfileAvatarLink/ProfileAvatar/ProfileAvatar.tsx
@@ -1,28 +1,37 @@
 import { useAuth } from "@/contexts/AuthContext/useAuth";
-import { FaRegUserCircle } from "@/assets/icons/icon";
+import { FaRegUserCircle, FaUserCircle } from "@/assets/icons/icon";
 import InitialsAvatar from "./InitialsAvatar/InitialsAvatar";
 
 /**
- * Renders the user's profile with the initials avatar
- *
- * If the user isn't authenticated displays an user icon
+ * Renders the user's profile avatar
  */
 export default function ProfileAvatar() {
   // Context
-  const { isProfileComplete } = useAuth();
+  const { authUser, isProfileComplete } = useAuth();
 
-  return (
-    <>
-      {isProfileComplete ? (
-        <InitialsAvatar />
-      ) : (
-        <FaRegUserCircle
-          className="text-3xl
+  // Unauthenticated user
+  if (!authUser?.user) {
+    return (
+      <FaRegUserCircle
+        className="text-3xl
                     text-brand
                     hover:text-brand-hover"
-          aria-hidden="true"
-        />
-      )}
-    </>
-  );
+        aria-hidden="true"
+      />
+    );
+  }
+
+  // Authenticated user with no user details (first and last name fields)
+  if (!isProfileComplete) {
+    return (
+      <FaUserCircle
+        className="text-3xl
+                text-brand
+                hover:text-brand-hover"
+        aria-hidden="true"
+      />
+    );
+  }
+
+  return <InitialsAvatar />;
 }

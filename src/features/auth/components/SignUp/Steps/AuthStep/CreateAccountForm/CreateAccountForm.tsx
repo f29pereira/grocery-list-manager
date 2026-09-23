efd14d878f3206ea527a89057e27aa3ead9f04a2
Filipe Pereira <f29.pereira@gmail.com>
2026-09-23 15:03:00 +0100

    feat: Update CreateAccountForm styles
    - For viewports >= 768px set max width to 450px and auto left/right margin
    - Add container with height of 70px for the SubmitErrorMessage
    - Remove margin and width styles for the CreateAccountButton container

--- src/features/auth/components/SignUp/Steps/AuthStep/CreateAccountForm/CreateAccountForm.tsx
+++ src/features/auth/components/SignUp/Steps/AuthStep/CreateAccountForm/CreateAccountForm.tsx
@@ -71,16 +71,18 @@ export default function CreateAccountForm() {
   return (
     <FormProvider {...methods}>
       <form
-        className="w-full"
+        className="md:max-w-112.5 md:mx-auto"
         onSubmit={methods.handleSubmit((data) => onSubmit(data))}
         noValidate
       >
         <EmailField validation={signUpEmailValidation} />
         <PasswordFieldWithRules validation={signUpPasswordValidation} />
 
-        <SubmitErrorMessage message={errorMessage} />
+        <div className="h-17.5">
+          <SubmitErrorMessage message={errorMessage} />
+        </div>
 
-        <div className="mt-2 sm:mx-auto sm:w-2/3 lg:w-full">
+        <div className="mt-2">
           <CreateAccountButton isSubmitting={isSubmitting} />
         </div>
       </form>

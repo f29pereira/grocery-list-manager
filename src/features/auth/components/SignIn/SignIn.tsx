af2c88868dd7f806f783b273e2b0f6674f9da1f7
Filipe Pereira <f29.pereira@gmail.com>
2026-09-23 10:11:59 +0100

    feat: Update SignIn styles
    - Make the main container a flex centered container and remove top/bottom padding
    - Card component: Add `w-full` and height of 642px

--- src/features/auth/components/SignIn/SignIn.tsx
+++ src/features/auth/components/SignIn/SignIn.tsx
@@ -16,12 +16,13 @@ export default function SignIn() {
 
   return (
     <div
-      className="min-h-screen px-6 py-10
+      className="flex justify-center items-center 
+                min-h-screen px-6
                 md:px-10
-                lg:flex lg:justify-center lg:items-center lg:p-0"
+                lg:p-0"
     >
       <Card
-        styles="relative px-6 py-10 
+        styles="relative w-full px-6 py-10 h-160.5
               sm:px-10 md:px-20
               lg:w-200 lg:h-187.5 lg:px-40"
       >

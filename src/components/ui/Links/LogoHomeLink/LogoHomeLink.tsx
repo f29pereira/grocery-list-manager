156dba96b7c1ae38f0115f526ec1e23bfbe5bc9b
Filipe Pereira <f29.pereira@gmail.com>
2026-09-25 12:13:24 +0100

    refactor: Replace the logo related elements with the Logo component on LogoHomeLink

--- src/components/ui/Links/LogoHomeLink/LogoHomeLink.tsx
+++ src/components/ui/Links/LogoHomeLink/LogoHomeLink.tsx
@@ -1,14 +1,8 @@
-import { TbPaperBag } from "@/assets/icons/icon";
 import NavigationLink from "@/components/ui/NavigationLink/NavigationLink";
+import Logo from "@/components/shared/Logo/Logo";
 
 /**
  * Renders the app logo as a navigation link
- *
- * Displays:
- * - the logo icon and "GM" on viewports < 640px
- * - the logo icon and "Grocery List" viewports >= 640px
- *
- * Props are defined in {@link LogoProps}.
  */
 export default function LogoHomeLink() {
   return (
@@ -20,18 +14,7 @@ export default function LogoHomeLink() {
             theme-transition"
       to="/"
     >
-      <div className="flex justify-center items-center gap-1">
-        <TbPaperBag className="text-2xl sm:text-3xl" aria-hidden="true" />
-
-        {/*Viewports < 640px*/}
-        <span className="sr-only md:hidden">Grocery List</span>
-        <span className="text-2xl tracking-widest md:hidden" aria-hidden="true">
-          GL
-        </span>
-
-        {/*Viewports >= 640px*/}
-        <span className="hidden md:inline md:text-3xl">Grocery List</span>
-      </div>
+      <Logo />
     </NavigationLink>
   );
 }

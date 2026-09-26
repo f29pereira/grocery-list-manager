2eba8f693f975f4b42a23637101b1adcb75d6f44
Filipe Pereira <f29.pereira@gmail.com>
2026-09-26 16:21:06 +0100

    refactor: Update routes folder with new folder auth and move to it the related components
    - Move the following components to the auth folder: AuthLayout, SignUpRoute, SignInRoute and ForgotPasswordRoute

--- src/routes/auth/authLayout.tsx
+++ src/routes/auth/authLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 
 /**
- * Renders the route content
+ * Renders the route content for the "sign-up", "sign-in" and "password-reset" routes
  */
 export default function AuthLayout() {
   return (

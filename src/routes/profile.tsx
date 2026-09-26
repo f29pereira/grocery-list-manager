2735cefab03182483366ed4db93e60083701b72a
Filipe Pereira <f29.pereira@gmail.com>
2026-09-26 16:24:51 +0100

    chore: Remove component ProfileScreen from routes folder

--- src/routes/profile.tsx
+++
@@ -1,8 +0,0 @@
-import ProfileManager from "@/features/auth/components/ProfileManager/ProfileManager";
-
-/**
- * Renders the ProfileManager component on the "profile" route
- */
-export default function ProfileScreen() {
-  return <ProfileManager />;
-}
